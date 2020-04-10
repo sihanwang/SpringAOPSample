791eb8a148720ec0c458aaeaa38d1a6a71d8e50d
sihanwang <ocpwj@hotmail.com>
2020-04-10 10:11:47 +0800

    init

---
+++ src/test/java/com/sihan/study/SpringAOPSample/SpringAopSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihan.study.SpringAOPSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
